d44bf4f6eb1abb2e48931c36998f53021313e3b5
Demondehell <Demondehell@gmail.com>
2022-09-22 20:46:03 +0700

    Opertor added

---
+++ Operator.py
@@ -0,0 +1,38 @@
+a = 5
+b = 3
+
+c = a+b
+d = a*b
+e = a/b
+f = a%b
+g = a**b
+h = a//b
+print("*****Operator*******")
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+print("*****Compare*******")
+print(a == b)
+print(a > b)
+print(a < b)
+print(a != b)
+print(a <= b)
+print(a >= b)
+print(a is b)
+print(a is not b)
+
+print("*****Logic*****")
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
+
+# Bitwise 
+# https://www.geeksforgeeks.org/python-operators/
